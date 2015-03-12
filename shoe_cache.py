f2e1452eed31329b914ece29438c15064d8d6124
GeekPower <siseulemen@gmail.com>
2015-03-12 19:45:24 +0800

    test

--- shoe_cache.py
+++ shoe_cache.py
@@ -1,9 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import redis
+import urllib2
 
+
+backed_image_url = 'http://tucoo.com/logo_class/telecom_logo04/images/Geek.png'
 
 app = Flask(__name__)
-r = redis.StrictRedis(host='localhost', port=6379)
+r = redis.StrictRedis(host='192.168.1.52', port=6379)
 
 default_upper = 'default_upper'
 # /image/?s=A02-B05-C02-H04&upper1=AH5
@@ -13,15 +16,14 @@ def cache_image():
         raise Exception('There is not the value of the "s" in the url')
     s = request.args['s']
     upper = request.args.get('upper1', default_upper)
-    key = str.join(s, ':', upper)
-    image = r.get(key)
-    if not image:
-        return image
+    key = s + ':' + upper
+    img_data = r.get(key)
+    if not img_data:
+        img_data = urllib2.urlopen(backed_image_url).read()
+        r.set(key, img_data)
+        return Response(img_data, content_type='image/jpeg')
     else:
-        r.set(key, key)
-        return key
-
-    return 'Hello World' + request.args['name'].get('upper1', default_upper)
+        return Response(img_data, content_type='image/jpeg')
 
 
 if __name__ == '__main__':
